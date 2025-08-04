001ced908b8129bdbc4774b842f1f128e3ab59d0
Sherin <csherin111@gmail.com>
2025-08-04 23:35:24 +0400

    splash screen styling added

--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -1,7 +1,8 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-
+import { LinearGradient } from "expo-linear-gradient";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../types/types";
 
@@ -12,10 +13,22 @@ type SplashScreenNavigationProp = NativeStackNavigationProp<
 
 export default function SplashScreen({ navigation }: any) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={["#06429F", "#9EDBF7"]}
+      style={styles.container}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 1 }}
+    >
+      <FontAwesome5
+        name="city"
+        size={100}
+        color="#ffffff"
+        style={styles.icon}
+      />
+
       <Text style={styles.title}>Welcome to City Pulse</Text>
       <Text style={styles.subtitle}>
-        Discover exciting local events near you!
+        Discover exciting local events near you !!
       </Text>
 
       <TouchableOpacity
@@ -24,39 +37,41 @@ export default function SplashScreen({ navigation }: any) {
       >
         <Text style={styles.buttonText}>Get Started</Text>
       </TouchableOpacity>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f8f9fa",
     justifyContent: "center",
     alignItems: "center",
     padding: 24,
   },
+  icon: {
+    marginBottom: 30,
+  },
   title: {
     fontSize: 32,
     fontWeight: "700",
-    color: "#2c3e50",
+    color: "#ffffff",
     marginBottom: 10,
     textAlign: "center",
   },
   subtitle: {
     fontSize: 18,
-    color: "#7f8c8d",
+    color: "#f0f0f0",
     textAlign: "center",
     marginBottom: 40,
   },
   button: {
-    backgroundColor: "#3498db",
+    backgroundColor: "#2c3e50",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 25,
   },
   buttonText: {
-    color: "#fff",
+    color: "#ffffff",
     fontSize: 18,
     fontWeight: "600",
   },
