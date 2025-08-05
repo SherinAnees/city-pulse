4e4ef77db291ae96c6ad9eab3da1f6a8562a4cb3
Sherin <csherin111@gmail.com>
2025-08-05 12:10:17 +0400

    signup screen styling updated

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -7,11 +7,14 @@ import {
   StyleSheet,
   Alert,
   TouchableOpacity,
+  Pressable,
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/store";
 import { signupUser } from "../redux/slices/authSlice";
-
+import GradientWrapper from "../components/GradientWrapper";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 export default function SignupScreen({ navigation }: any) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -25,7 +28,11 @@ export default function SignupScreen({ navigation }: any) {
       ).unwrap();
       navigation.replace("Home");
     } catch (error) {
-      console.error("Login failed:", error);
+      console.error("Signup failed:", error);
+      Alert.alert(
+        "Signup Failed",
+        (error as Error)?.message || "Invalid email or password."
+      );
     }
   };
   useEffect(() => {
@@ -34,30 +41,56 @@ export default function SignupScreen({ navigation }: any) {
     }
   }, [user]);
   return (
-    <View style={styles.container}>
+    <GradientWrapper style={styles.container}>
       <Text style={styles.title}>Sign Up</Text>
+      <View style={styles.inputContainer}>
+        <SimpleLineIcons
+          name="user"
+          size={20}
+          color="#555"
+          style={styles.icon}
+        />
+        <TextInput
+          placeholder="Name"
+          value={name}
+          onChangeText={setName}
+          style={styles.input}
+        />
+      </View>
 
-      <TextInput
-        placeholder="Name"
-        value={name}
-        onChangeText={setName}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        style={styles.input}
-      />
-
-      <Button title="Sign Up" onPress={handleSignup} />
+      <View style={styles.inputContainer}>
+        <Ionicons
+          name="mail-outline"
+          size={20}
+          color="#555"
+          style={styles.icon}
+        />
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+          keyboardType="email-address"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons
+          name="lock-closed-outline"
+          size={20}
+          color="#555"
+          style={styles.icon}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          style={styles.input}
+        />
+      </View>
+      <Pressable style={styles.button} onPress={handleSignup}>
+        <Text style={styles.buttonText}>Sign p</Text>
+      </Pressable>
 
       <TouchableOpacity onPress={() => navigation.navigate("Login")}>
         <Text style={styles.link}>Already have an account? Login</Text>
@@ -65,20 +98,64 @@ export default function SignupScreen({ navigation }: any) {
       <TouchableOpacity onPress={() => navigation.navigate("Home")}>
         <Text style={styles.subLine}>Continue as Guest</Text>
       </TouchableOpacity>
-    </View>
+    </GradientWrapper>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", paddingHorizontal: 20 },
-  title: { fontSize: 28, textAlign: "center", marginBottom: 20 },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    marginBottom: 15,
-    padding: 10,
-    borderRadius: 8,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
   },
-  link: { marginTop: 20, color: "#007AFF", textAlign: "center" },
-  subLine: { marginTop: 20, color: "#f6307fff", textAlign: "center" },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff",
+    marginBottom: 40,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    elevation: 2,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 14,
+    borderRadius: 25,
+    marginTop: 10,
+    elevation: 3,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  link: {
+    marginTop: 20,
+    color: "#24173aaa",
+    textAlign: "center",
+    fontSize: 14,
+    textDecorationLine: "underline",
+  },
+  subLine: {
+    marginTop: 15,
+    color: "#4d0202aa",
+    textAlign: "center",
+    fontSize: 14,
+  },
 });
